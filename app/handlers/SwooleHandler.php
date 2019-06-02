55608f446c4d74b22c12228765c1c2c1f15ef62c
lxj <864969742@qq.com>
2019-06-02 10:50:09 +0800

    心跳

--- app/handlers/SwooleHandler.php
+++ app/handlers/SwooleHandler.php
@@ -27,7 +27,6 @@ class SwooleHandler
 	 */
 	public function onOpen(\swoole_websocket_server $ws, $request)
 	{
-	    echo '213';
 		$Redis = new RedisServer();
 		$Redis->rPush('connect',$request->fd);
 		$arr = [
